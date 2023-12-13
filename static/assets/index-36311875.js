fb09a78f147630881ed60d174c07c283d32e79af
明扬 <my@famer.me>
2023-12-13 21:24:04 +0800

    update jinyu FE
    Signed-off-by: 明扬 <my@famer.me>

---
+++ static/assets/index-36311875.js
@@ -0,0 +1,1 @@
+import{d as e,o as n,c as o}from"./index-b30faf5d.js";const p=e({name:"ViewSignUp",__name:"index",setup(t){return(a,c)=>(n(),o("div"))}});export{p as default};
--- static/assets/index-68c151f5.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o as n,c as o}from"./index-4fc82776.js";const p=e({name:"ViewSignUp",__name:"index",setup(t){return(a,c)=>(n(),o("div"))}});export{p as default};
